da8bcf594c7c3cd1b59ae8a6a5e20eb66511f321
Partha <Partha@gotoowild.com>
2020-05-11 19:13:40 -0400

    May 11th - 7:13

--- LootLogger/ItemStore.swift
+++ LootLogger/ItemStore.swift
@@ -10,31 +10,34 @@ import UIKit
 
 class ItemStore {
     
-    var allItems = [Item]()
-    
+    var allItems = [[Item](),[Item]()]
+    var sections = ["Price > $50", "Price < $50"]
     
     @discardableResult func createItem() -> Item{
         let newItem = Item(random: true)
-        allItems.append(newItem)
+        if newItem.valueInDollars >= 50 {
+        allItems[0].append(newItem)
+        } else {
+            allItems[1].append(newItem)
+        }
+         
         return newItem
     }
     
     func removeItem(_ item: Item){
-        if let index = allItems.firstIndex(of: item){
-            allItems.remove(at: index)
-        }
+        let sectionIndex = item.valueInDollars >= 50 ? 0 : 1
+        if let index = allItems[sectionIndex].firstIndex(of: item){
+                    allItems[sectionIndex].remove(at: index)
+          }
     }
     
-    func moveItems(from fromIndex:Int, to toIndex:Int) {
-        
-        if(fromIndex == toIndex){
+    func moveItems(from fromIndex:Int, to toIndex:Int, fromSection fromSectionIndex:Int, toSection toSectionIndex:Int) {
+        if(fromSectionIndex == toSectionIndex) && (fromIndex == toIndex){
             return
         }
-        
-        let moveItem = allItems[fromIndex]
-        
-        allItems.remove(at: fromIndex)
-        allItems.insert(moveItem, at: toIndex)
+        let moveItem = allItems[fromSectionIndex][fromIndex]
+        allItems[fromSectionIndex].remove(at: fromIndex)
+        allItems[toSectionIndex].insert(moveItem, at: toIndex)
     }
      
 }
--- LootLogger/ItemsViewController.swift
+++ LootLogger/ItemsViewController.swift
@@ -16,49 +16,96 @@ class ItemsViewController: UITableViewController {
     @IBAction func addNewItem(_ sender:UIButton) {
         
         let newItem = itemStore.createItem()
-        
-        if let index = itemStore.allItems.firstIndex(of: newItem){
-             let indexPath = IndexPath(row: index, section: 0)
-            tableView.insertRows(at: [indexPath], with: .automatic)
+        let sectionIndex = newItem.valueInDollars >= 50 ? 0 : 1
+            if let index = itemStore.allItems[sectionIndex].firstIndex(of: newItem){
+                 let indexPath = IndexPath(row: index, section: sectionIndex)
+                tableView.insertRows(at: [indexPath], with: .automatic)
         }
     }
     
     @IBAction func toggleEditingMode(_ sender: UIButton) {
         if isEditing{
-
             sender.setTitle("Edit", for: .normal)
             setEditing(false, animated: true)
         } else {
-
+            if (itemStore.allItems[0].count == 0) && (itemStore.allItems[1].count == 0) {
+                return
+            } else {
             sender.setTitle("Done", for: .normal)
             setEditing(true, animated: true)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var sectionHeader = ""
+        if (itemStore.allItems[0].count == 0) && (itemStore.allItems[1].count == 0) {
+            sectionHeader = "There are no items"
+            return sectionHeader
+        } else {
+        if section == 0 {
+            sectionHeader = itemStore.sections[0]
+        } else {
+            sectionHeader = itemStore.sections[1]
+        }
+        return sectionHeader
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
-        return itemStore.allItems.count
+        if itemStore.allItems[section].count == 0 {
+            return 1
+        } else {
+        return itemStore.allItems[section].count + 1
+        }
     }
     
     override func tableView(_ tableView:UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
-        
-        let item = itemStore.allItems[indexPath.row]
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-        return cell
+       
+        let noItemCell = tableView.dequeueReusableCell(withIdentifier: "NoItemCell")!
+
+        if (itemStore.allItems[0].count == 0) && (itemStore.allItems[1].count == 0) {
+            noItemCell.textLabel?.text = "No Items Found"
+             return noItemCell
+        } else {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+            let item = itemStore.allItems[indexPath.section][indexPath.row]
+            cell.textLabel?.text = item.name
+            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+             return cell
+        }
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+//        if (itemStore.allItems[0].count == 0) && (itemStore.allItems[1].count == 0) {
+//            cell.textLabel?.text = "No Items Found"
+//        } else {
+//            let item = itemStore.allItems[indexPath.section][indexPath.row]
+//            cell.textLabel?.text = item.name
+//            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+//        }
+//        return cell
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            
-            let item = itemStore.allItems[indexPath.row]
+            let item = itemStore.allItems[indexPath.section][indexPath.row]
             itemStore.removeItem(item)
             tableView.deleteRows(at: [indexPath], with: .automatic)
-            
         }
     }
     
     override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        itemStore.moveItems(from: sourceIndexPath.row, to: destinationIndexPath.row)
+        return
+       // itemStore.moveItems(from: sourceIndexPath.row, to: destinationIndexPath.row, fromSection: sourceIndexPath.section, toSection: destinationIndexPath.section)
     }
+    
+    
 }
